0fa502002f88a01a8e2663eea2ea99646151c8c3
FaryalFaisal29 <164797311+FaryalFaisal29@users.noreply.github.com>
2024-03-26 01:35:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+// choosing what to do on your day off based on the day. If it's Saturday, you go for hiking. If it's Sunday, you read a book, otherwise, do gardening.
+let dayOff = "Tuesday";
+switch (dayOff) {
+    case "Saturday":
+        console.log("Go for hiking.");
+        break;
+    case "Sunday":
+        console.log("Read a book.");
+        break;
+    default:
+        console.log("Do Gardening.");
+}
+let favFruit = "banana";
+switch (favFruit) {
+    case "mango":
+        console.log("Mango is the king of all fruits.");
+        break;
+    case "orange":
+        console.log("Orange is full of vitamin c.");
+        break;
+    case "banana":
+        console.log("Momkeys love to eat bananas, so as kids too.");
+        break;
+    default:
+        console.log("This fruit is red and sweet.");
+}
+export {};
